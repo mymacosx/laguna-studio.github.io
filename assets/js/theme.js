3f7d7c7dd8ca51ea2e0feb61973bf020b7f29bfb
Ukr55 <ukr55@me.com>
2019-03-16 14:25:33 +0200

    ddf
    fggdf

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,6 +1,6 @@
 $( document ).ready(function( $ ) {
 		$( '#slide-1' ).sliderPro({
-			width: 300,
+            width: 400,
 			height: 400,
 			orientation: 'horizontal',
 			loop: false,
@@ -9,11 +9,14 @@ $( document ).ready(function( $ ) {
 			buttons: false,
 			thumbnailsPosition: 'left',
 			thumbnailPointer: false,
+            aspectRatio: 1,
+			//visibleSize: '60%',
+			//forceSize: 'fullWidth'
             fullScreen: true,
             shuffle: true,
             smallSize: 500,
-			mediumSize: 800,
-			largeSize: 1200,
+			mediumSize: 1000,
+			largeSize: 3000,
 			thumbnailArrows: false,
 			autoplay: false,
 			thumbnailWidth: 60,
@@ -24,19 +27,31 @@ $( document ).ready(function( $ ) {
                     thumbnailPointer: false,
                     thumbnailArrows: false,
                     arrows: false,
-					thumbnailWidth: 60,
-					thumbnailHeight: 60
+					thumbnailWidth: 0,
+					thumbnailHeight: 0
 				},
 				500: {
 					thumbnailsPosition: 'bottom',
                     thumbnailPointer: false,
                     thumbnailArrows: false,
                     arrows: false,
-					thumbnailWidth: 60,
-					thumbnailHeight: 60
+					thumbnailWidth: 0,
+					thumbnailHeight: 0
 				}
 			}
-		});    
+		});
+        // instantiate fancybox when a link is clicked
+		$( '#example2 .sp-image' ).parent( 'a' ).on( 'click', function( event ) {
+			event.preventDefault();
+
+			// check if the clicked link is also used in swiping the slider
+			// by checking if the link has the 'sp-swiping' class attached.
+			// if the slider is not being swiped, open the lightbox programmatically,
+			// at the correct index
+			if ( $( '#example2' ).hasClass( 'sp-swiping' ) === false ) {
+				$.fancybox.open( $( '#example2 .sp-image' ).parent( 'a' ), { index: $( this ).parents( '.sp-slide' ).index() } );
+			}
+		});
 	});
 
 
