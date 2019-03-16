5c2e519668b5b6e5b90f55f11c1f9c06dd9b8f31
Ukr55 <ukr55@me.com>
2019-03-16 11:30:18 +0200

    арпар
    прапрап

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,19 +1,42 @@
-// Custome theme code
-
-if ($('.clean-gallery').length > 0) {
-   baguetteBox.run('.clean-gallery', { animation: 'slideIn'});
-}
-
-if ($('.clean-product').length > 0) {
-    $(window).on("load",function() {
-        $('.sp-wrap').smoothproducts();
-    });
-}
-
-$(document).ready(function() {
-    $('.pgwSlider').pgwSlider();
-});
-
-
+$( document ).ready(function( $ ) {
+		$( '#slide-1' ).sliderPro({
+			width: 300,
+			height: 400,
+			orientation: 'horizontal',
+			loop: false,
+            fade: true,
+			arrows: true,
+			buttons: false,
+			thumbnailsPosition: 'left',
+			thumbnailPointer: false,
+            fullScreen: true,
+            shuffle: true,
+            smallSize: 500,
+			mediumSize: 800,
+			largeSize: 1200,
+			thumbnailArrows: false,
+			autoplay: false,
+			thumbnailWidth: 60,
+            thumbnailHeight: 60,
+			breakpoints: {
+				800: {
+					thumbnailsPosition: 'bottom',
+                    thumbnailPointer: false,
+                    thumbnailArrows: false,
+                    arrows: false,
+					thumbnailWidth: 60,
+					thumbnailHeight: 60
+				},
+				500: {
+					thumbnailsPosition: 'bottom',
+                    thumbnailPointer: false,
+                    thumbnailArrows: false,
+                    arrows: false,
+					thumbnailWidth: 60,
+					thumbnailHeight: 60
+				}
+			}
+		});    
+	});
 
 
